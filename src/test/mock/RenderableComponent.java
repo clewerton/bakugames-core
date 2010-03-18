9a60af15025514d30a19d7b088edbc757469dad0
h.anjos <h.anjos@9512e85f-ba90-a8ac-3d14-19c2b51aa562>
2010-03-18 11:35:21 +0000

    Moving input validity checks to Entity's protected methods (now plug, unplug and swap just delegate)

---
+++ src/test/mock/RenderableComponent.java
@@ -0,0 +1,25 @@
+package mock;
+
+import org.bakugames.core.Component;
+import org.bakugames.core.Entity;
+import org.bakugames.core.Renderable;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class RenderableComponent extends Component implements Renderable {
+  public int renderCount = 0;
+  
+  public RenderableComponent(String id) {
+    super(id);
+  }
+  
+  public RenderableComponent(String id, Entity owner) {
+    super(id, owner);
+  }
+
+  @Override
+  public void render(GameContainer gc, StateBasedGame sb, Graphics gr) {
+    renderCount++;
+  }
+}
