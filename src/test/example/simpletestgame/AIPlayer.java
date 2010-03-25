8e99b32873e123e3cb557ddbcf965c62d9a29eb7
h.anjos <h.anjos@9512e85f-ba90-a8ac-3d14-19c2b51aa562>
2010-03-25 11:58:43 +0000

    Putting the example game in example.simpletestgame.

---
+++ src/test/example/simpletestgame/AIPlayer.java
@@ -0,0 +1,57 @@
+package example.simpletestgame;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.bakugames.core.Entity;
+import org.bakugames.input.Player;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class AIPlayer implements Player {
+  private enum Step {
+    RUN(300, "run"), TURN_RIGHT(226, "turn right");
+    
+    private int time;
+    private int delay;
+    private String instruction;
+
+    private Step(int delay, String instruction) {
+      this.delay = delay;
+      this.instruction = instruction;
+      
+      time = 0;
+    }
+    
+    public boolean execute(Entity entity, int delta) {
+      time += delta;
+      
+      if(time < delay) {
+        entity.execute(instruction);
+        return false;
+      }
+      
+      time = 0;
+      return true;
+    }
+  }
+  
+  private Entity entity;
+  private List<Step> script;
+  private int cursor;
+  
+  public AIPlayer(Entity entity) {
+    this.entity = entity;
+    
+    script = Arrays.asList(Step.RUN, Step.TURN_RIGHT);
+    cursor = 0;
+  }
+
+  @Override
+  public void update(GameContainer gc, StateBasedGame sb, int delta) {
+    Step step = script.get(cursor);
+    
+    if(step.execute(entity, delta))
+      cursor = (cursor + 1) % script.size();
+  }
+}
---
+++ src/test/example/simpletestgame/KeyDownControl.java
@@ -0,0 +1,40 @@
+package example.simpletestgame;
+
+import org.bakugames.input.Control;
+import org.newdawn.slick.Input;
+
+public class KeyDownControl implements Control {
+  private int key;
+  
+  public KeyDownControl(int key) {
+    this.key = key;
+  }
+
+  // operations
+  @Override
+  public boolean happened(Input input) {
+    return input.isKeyDown(getKey());
+  }
+  
+  // utility methods
+  @Override
+  public boolean equals(Object o) {
+    if(o == this)
+      return true;
+    
+    if(! (o instanceof KeyDownControl))
+      return false;
+    
+    return getKey() == ((KeyDownControl) o).getKey();
+  }
+  
+  @Override
+  public int hashCode() {
+    return getKey();
+  }
+  
+  // properties
+  public int getKey() {
+    return key;
+  }
+}
