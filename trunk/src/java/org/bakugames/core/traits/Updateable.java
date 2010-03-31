0ae786144226c01d42ba9cc0f32e6aadb8354b5f
h.anjos <h.anjos@9512e85f-ba90-a8ac-3d14-19c2b51aa562>
2010-03-31 16:47:04 +0000

    Restructuring the project.

--- trunk/src/java/org/bakugames/core/traits/Updateable.java
+++
@@ -1,8 +0,0 @@
-package org.bakugames.core.traits;
-
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.state.StateBasedGame;
-
-public interface Updateable {
-  void update(GameContainer gc, StateBasedGame sb, int delta);
-}
